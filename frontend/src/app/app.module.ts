0625c08d23507a1143ce9c0c783a2389107533f1
Paulina Ostroga <paulinaa.ostr@gmail.com>
2020-07-28 15:12:07 +0200

    new components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorsListComponent } from './administrator/doctors-list/doctors-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './administrator/main/main.component';
+import { SearchBarComponent } from './pacient/search-bar/search-bar.component';
+import { VisitListComponent } from './pacient/visit-list/visit-list.component';
 
 @NgModule({
-	declarations: [AppComponent, DoctorsListComponent, MainComponent],
+	declarations: [AppComponent, DoctorsListComponent, MainComponent, SearchBarComponent, VisitListComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
