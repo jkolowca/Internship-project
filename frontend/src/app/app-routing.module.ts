b68a4b27f3b573110afb582ee441a4bb15b1852b
Joanna Kolowca <joanna.kolowca@gmail.com>
2020-07-28 14:29:01 +0200

    folder structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DoctorsListComponent } from './doctors-list/doctors-list.component';
+import { MainComponent as adm } from './administrator/main/main.component';
+import { MainComponent as doc } from './doctor/main/main.component';
+import { MainComponent as pac } from './pacient/main/main.component';
 
-const routes: Routes = [{ path: '', component: DoctorsListComponent }];
+const routes: Routes = [
+	{ path: 'adm', component: adm },
+	{ path: 'doc', component: doc },
+	{ path: 'pac', component: pac },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DoctorsListComponent } from './doctors-list/doctors-list.component';
+import { DoctorsListComponent } from './administrator/doctors-list/doctors-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './administrator/main/main.component';
 
 @NgModule({
-	declarations: [AppComponent, DoctorsListComponent],
+	declarations: [AppComponent, DoctorsListComponent, MainComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
