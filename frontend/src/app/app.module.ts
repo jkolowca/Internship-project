85a1681ee9a74bb327ce62e356f8aaf3a17f9b5e
Joanna Kolowca <joanna.kolowca@gmail.com>
2020-07-28 15:10:18 +0200

    doctors service added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DoctorsListComponent } from './administrator/doctors-list/doctors-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './administrator/main/main.component';
 
 @NgModule({
---
+++ frontend/src/app/services/doctors.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Doctor } from '../interfaces/doctor';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class DoctorsService {
+	private doctorsUrl = 'http://localhost:5000/doctors';
+	httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+	};
+
+	constructor(private http: HttpClient) {}
+
+	getAll(): Observable<Doctor[]> {
+		return this.http
+			.get<Doctor[]>(this.doctorsUrl, this.httpOptions)
+			.pipe(catchError(this.handleError<Doctor[]>('getAll', [])));
+	}
+
+	add(
+		name: string,
+		surname: string,
+		specialties: string[],
+		clinics: string[]
+	): Observable<any> {
+		return this.http
+			.post<Doctor>(
+				this.doctorsUrl,
+				{ name, surname, specialties, clinics },
+				this.httpOptions
+			)
+			.pipe(
+				tap((newDoctor: Doctor) =>
+					console.log(`added doctor id=${newDoctor._id}`)
+				),
+				catchError(this.handleError<Doctor>('add'))
+			);
+	}
+
+	private handleError<T>(
+		operation?: string,
+		result?: T
+	): (a: any) => Observable<T> {
+		return (error: any): Observable<T> => {
+			console.error(`${error} at: ${operation}`);
+			return of(result as T);
+		};
+	}
+}
