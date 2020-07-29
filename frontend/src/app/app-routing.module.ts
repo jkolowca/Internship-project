88cb1ed098291ad69821e5590b51be4dea3b5f69
Paulina Ostroga <paulinaa.ostr@gmail.com>
2020-07-29 14:51:25 +0200

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent as adm } from './administrator/main/main.component';
 import { MainComponent as doc } from './doctor/main/main.component';
 import { MainComponent as pac } from './pacient/main/main.component';
+import { RegistrationFormComponent } from './pacient/registration-form/registration-form.component';
 
 const routes: Routes = [
 	{ path: 'adm', component: adm },
 	{ path: 'doc', component: doc },
 	{ path: 'pac', component: pac },
+	{ path: 'pac/registration', component: RegistrationFormComponent}
 ];
 
 @NgModule({
