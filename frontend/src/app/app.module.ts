4644cbf069f454574c4aff6161196b205a336af2
Paulina Ostroga <paulinaa.ostr@gmail.com>
2020-07-29 12:58:26 +0200

    material module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,27 +3,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material.module';
-
 import { AppComponent } from './app.component';
-
 import { MainComponent as AdministratorComponent } from './administrator/main/main.component';
 import { DoctorsListComponent } from './administrator/doctors-list/doctors-list.component';
-
 import { MainComponent as DoctorComponent } from './doctor/main/main.component';
-
 import { MainComponent as PacientComponent } from './pacient/main/main.component';
 import { SearchBarComponent } from './pacient/search-bar/search-bar.component';
 import { VisitListComponent } from './pacient/visit-list/visit-list.component';
 import { DoctorFormComponent } from './administrator/doctor-form/doctor-form.component';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { RegistrationFormComponent } from './pacient/registration-form/registration-form.component';
 
 @NgModule({
@@ -43,16 +31,7 @@ import { RegistrationFormComponent } from './pacient/registration-form/registrat
 		BrowserModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
-		FormsModule,
-		ReactiveFormsModule,
-		MaterialModule,
-		MatCardModule,
-		MatFormFieldModule,
-		MatSelectModule,
-		MatInputModule,
-		MatButtonModule,
-		FormsModule,
-		ReactiveFormsModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
--- frontend/src/app/shared/material.module.ts
+++ frontend/src/app/shared/material.module.ts
@@ -4,6 +4,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
 	exports: [
@@ -12,6 +17,10 @@ import { MatButtonModule } from '@angular/material/button';
 		MatFormFieldModule,
 		MatInputModule,
 		MatButtonModule,
+		MatCardModule,
+		MatSelectModule,
+		FormsModule,
+		ReactiveFormsModule
 	],
 })
 export class MaterialModule {}
