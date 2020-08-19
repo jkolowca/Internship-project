c5947af125b309e7d328913c525c9fab2b25e78e
Paulina Ostroga <paulinaa.ostr@gmail.com>
2020-08-19 09:10:48 +0200

    patient registration form

--- frontend/src/app/patient/components/patient/registration-form/registration-form.component.ts
+++ frontend/src/app/patient/components/patient/registration-form/registration-form.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { VisitsService } from 'src/app/shared/services/visits.service';
 import { Appointment } from '../../../../../../../common/interfaces';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
 	selector: 'app-registration-form',
@@ -14,33 +15,30 @@ export class RegistrationFormComponent implements OnInit {
 	registrationForm: FormGroup;
 	visitId = this.route.snapshot.paramMap.get('id');
 	userId = this.route.parent.snapshot.paramMap.get('id');
-
+	surname : string;
+	name: string; 
 	constructor(
 		private visitsService: VisitsService,
 		private route: ActivatedRoute,
 		private snackBar: MatSnackBar,
 		public fb: FormBuilder,
-		public router: Router
+		public router: Router,
+		private authService: AuthService
 	) {}
 
 	ngOnInit(): void {
 		this.registrationForm = this.fb.group({
-			name: ['', [Validators.required]],
-			surname: ['', [Validators.required]],
 			reason: '',
 		});
-	}
-
-	getErrorMessage(prop): string {
-		if (prop.hasError('required')) {
-			return 'You must enter a value';
-		}
+		this.authService.getUserProfile(this.userId).subscribe( l => {this.name = l.name, this.surname = l.surname}
+			);
 	}
 
 	register(): void {
 		const appointment: Appointment = this.registrationForm.value;
 		appointment._id = this.userId;
-
+		appointment.name = this.name;
+		appointment.surname = this.surname;
 		this.visitsService.register(this.visitId, appointment).subscribe(
 			() => {
 				this.snackBar.open('A doctor visit was agreed', 'End', {
